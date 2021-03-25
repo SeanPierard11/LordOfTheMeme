399e21f8c6c655938019068017036fd910ab9120
SeanPierard11 <80447744+SeanPierard11@users.noreply.github.com>
2021-03-25 18:58:22 +0100

    La base est faite

---
+++ LordOfTheMemeUnity/Assets/script/Player/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float MoveSpeed;
+    public LayerMask SolidObjectLayer;
+    public LayerMask LongGrassLayer;
+
+    private bool IsMoving;
+    private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (!IsMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (IsWalkable(targetPos))
+                {
+                    StartCoroutine(Move(targetPos));
+
+                }
+            }
+        }
+        animator.SetBool("IsMoving", IsMoving);
+    }
+
+    IEnumerator Move(Vector3 targetpos)
+    {
+        IsMoving = true;
+
+        while ((targetpos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetpos, MoveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetpos;
+        IsMoving = false;
+
+        CheckForEncounters();
+    }
+
+    private bool IsWalkable(Vector3 targetpos)
+    {
+        if(Physics2D.OverlapCircle(targetpos, 0.3f, SolidObjectLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, LongGrassLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("encountered a wild meme");
+            }
+        }
+    }
+}
